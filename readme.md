956af0ffffb01ce0cd0514d453b188c80fd8b42f
rveres <rveres@ethz.ch>
2022-11-28 21:22:25 +0100

    First commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+Okay so you want to start this project?
+
+Just go:
+```$pip install -r requirements.txt```
+
+Then:
+```python water_shooting.py```
+# Fluid-Directed-Rigid-Body-Control-with-DeepRL
+# Fluid-Directed-Rigid-Body-Control-with-DeepRL
---
+++ water_shooting.py
@@ -0,0 +1,226 @@
+import taichi as ti
+
+ti.init(arch=ti.gpu)  # Try to run on GPU
+
+
+#______________Convenience parameters
+RESOLUTION = 512
+quality = 1  # Use a larger value for higher-res simulations
+n_particles, n_grid = 9000 * quality**2, 64 * quality
+jet_speed = 0.05
+jet_angular_speed =  jet_speed * ti.math.pi
+ball_radius = 0.2
+jet_r = ball_radius/2
+jet_power = 20
+shoot_length = 20 #Number of frames until a "shot" takes
+#_______________
+dx, inv_dx = 1 / n_grid, float(n_grid)
+dt = 1e-4 / quality
+p_vol, p_rho = (dx * 0.5)**2, 1
+p_mass = p_vol * p_rho
+E, nu = 5e3, 0.2  # Young's modulus and Poisson's ratio
+mu_0, lambda_0 = E / (2 * (1 + nu)), E * nu / (
+    (1 + nu) * (1 - 2 * nu))  # Lame parameters
+
+x = ti.Vector.field(2, dtype=float, shape=n_particles)  # position
+v = ti.Vector.field(2, dtype=float, shape=n_particles)  # velocity
+C = ti.Matrix.field(2, 2, dtype=float,
+                    shape=n_particles)  # affine velocity field
+F = ti.Matrix.field(2, 2, dtype=float,
+                    shape=n_particles)  # deformation gradient
+material = ti.field(dtype=int, shape=n_particles)  # material id
+Jp = ti.field(dtype=float, shape=n_particles)  # plastic deformation
+grid_v = ti.Vector.field(2, dtype=float,
+                         shape=(n_grid, n_grid))  # grid node momentum/velocity
+grid_m = ti.field(dtype=float, shape=(n_grid, n_grid))  # grid node mass
+gravity = ti.Vector.field(2, dtype=float, shape=())
+attractor_strength = ti.field(dtype=float, shape=())
+attractor_pos = ti.Vector.field(2, dtype=float, shape=())
+jet_attributes = ti.Vector.field(2, dtype=float, shape=())
+
+
+@ti.kernel
+def substep():
+    for i, j in grid_m:
+        grid_v[i, j] = [0, 0]
+        grid_m[i, j] = 0
+    for p in x:  # Particle state update and scatter to grid (P2G)
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1)**2, 0.5 * (fx - 0.5)**2]
+        # deformation gradient update
+        F[p] = (ti.Matrix.identity(float, 2) + dt * C[p]) @ F[p]
+        # Hardening coefficient: snow gets harder when compressed
+        h = max(0.1, min(5, ti.exp(10 * (1.0 - Jp[p]))))
+        if material[p] == 1:  # jelly, make it softer
+            h = 0.3
+        mu, la = mu_0 * h, lambda_0 * h
+        if material[p] == 0:  # liquid
+            mu = 0.0
+        U, sig, V = ti.svd(F[p])
+        J = 1.0
+        for d in ti.static(range(2)):
+            new_sig = sig[d, d]
+            if material[p] == 2:  # Snow
+                new_sig = min(max(sig[d, d], 1 - 2.5e-2),
+                              1 + 4.5e-3)  # Plasticity
+            Jp[p] *= sig[d, d] / new_sig
+            sig[d, d] = new_sig
+            J *= new_sig
+        if material[p] == 0:
+            # Reset deformation gradient to avoid numerical instability
+            F[p] = ti.Matrix.identity(float, 2) * ti.sqrt(J)
+        elif material[p] == 2:
+            # Reconstruct elastic deformation gradient after plasticity
+            F[p] = U @ sig @ V.transpose()
+        stress = 2 * mu * (F[p] - U @ V.transpose()) @ F[p].transpose(
+        ) + ti.Matrix.identity(float, 2) * la * J * (J - 1)
+        stress = (-dt * p_vol * 4 * inv_dx * inv_dx) * stress
+        affine = stress + p_mass * C[p]
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            # Loop over 3x3 grid node neighborhood
+            offset = ti.Vector([i, j])
+            dpos = (offset.cast(float) - fx) * dx
+            weight = w[i][0] * w[j][1]
+            grid_v[base + offset] += weight * (p_mass * v[p] + affine @ dpos)
+            grid_m[base + offset] += weight * p_mass
+    for i, j in grid_m:
+        if grid_m[i, j] > 0:  # No need for epsilon here
+            # Momentum to velocity
+            grid_v[i, j] = (1 / grid_m[i, j]) * grid_v[i, j]
+            grid_v[i, j] += dt * gravity[None] * 30  # gravity
+            dist = attractor_pos[None] - dx * ti.Vector([i, j])
+            grid_v[i, j] += \
+                dist / (0.01 + dist.norm()) * attractor_strength[None] * dt * 100
+            if i < 3 and grid_v[i, j][0] < 0:
+                grid_v[i, j][0] = 0  # Boundary conditions
+            if i > n_grid - 3 and grid_v[i, j][0] > 0:
+                grid_v[i, j][0] = 0
+            if j < 3 and grid_v[i, j][1] < 0:
+                grid_v[i, j][1] = 0
+            if j > n_grid - 3 and grid_v[i, j][1] > 0:
+                grid_v[i, j][1] = 0
+    for p in x:  # grid to particle (G2P)
+        base = (x[p] * inv_dx - 0.5).cast(int)
+        fx = x[p] * inv_dx - base.cast(float)
+        w = [0.5 * (1.5 - fx)**2, 0.75 - (fx - 1.0)**2, 0.5 * (fx - 0.5)**2]
+        new_v = ti.Vector.zero(float, 2)
+        new_C = ti.Matrix.zero(float, 2, 2)
+        for i, j in ti.static(ti.ndrange(3, 3)):
+            # loop over 3x3 grid node neighborhood
+            dpos = ti.Vector([i, j]).cast(float) - fx
+            g_v = grid_v[base + ti.Vector([i, j])]
+            weight = w[i][0] * w[j][1]
+            new_v += weight * g_v
+            new_C += 4 * inv_dx * weight * g_v.outer_product(dpos)
+        v[p], C[p] = new_v, new_C
+        x[p] += dt * v[p]  # advection
+
+
+
+
+@ti.kernel
+def reset():
+    """
+    create random position, for the ball we randomize polar coords and recast them to normal ones 
+    """
+    group_size = n_particles // 2
+    #Init ball
+    ratio = 0.4
+    for i in range(ratio*group_size):
+        radius = ti.random() * ball_radius
+        degree = ti.random() * 360 
+        spread = 0.1
+        new_x = ti.math.cos(degree)*radius*spread  + 0.5
+        new_y = ti.math.sin(degree)*radius*spread  + 0.5
+        x[i] = [new_x, new_y]
+        material[i] = 1 # 0: fluid 1: jelly 2: snow
+        v[i] = [0, 0]
+        F[i] = ti.Matrix([[1, 0], [0, 1]])
+        Jp[i] = 1
+        C[i] = ti.Matrix.zero(float, 2, 2)
+    #Init water
+    #TODO: think about paramaters
+    for i in range(ratio*group_size, n_particles):
+        radius = ti.random() * ball_radius
+        degree = ti.random() * 2*ti.math.pi
+        spread = 0.1
+        new_x = ti.math.cos(degree)*radius*spread  + 0.3 + 0.10 * 0 #(i // group_size)
+        new_y = ti.math.sin(degree)*radius*spread  + 0.05 + 0.32 * 0
+        x[i] = [new_x, new_y]
+        material[i] = 0 # 0: fluid 1: jelly 2: snow
+        v[i] = [0, 0]
+        F[i] = ti.Matrix([[1, 0], [0, 1]])
+        Jp[i] = 1
+        C[i] = ti.Matrix.zero(float, 2, 2)  
+    #Init jet
+    jet_attributes[None] = [0.5, ti.math.pi/2]
+
+@ti.kernel
+def random_pos() -> ti.f32:
+    return ti.random()
+
+
+gui = ti.GUI("Neil's and Robert's fun project uwu", res=RESOLUTION, background_color=0xACACAC)
+reset()
+gravity[None] = [0, -1]
+particle_shot_counter = 0
+for frame in range(20000):
+    print(x[0])
+    """
+    Managing gui inputs
+    r - Reset to initial state
+    esc - quits the program
+    any other button - sets the graviry to 0,1 xd
+    LMB - Shoot dihydrogen monoxide from the Particle accelerator
+    RMB - Left for debugging haha (secret)
+    A/D - move the ~jet~ particle accelerator to the left/right
+    """
+    if gui.get_event(ti.GUI.PRESS):
+        if gui.event.key == 'r':
+            reset()
+        elif gui.event.key in [ti.GUI.ESCAPE, ti.GUI.EXIT]:
+            break
+        elif gui.event.key == 'a':
+            tmp = ti.math.max(jet_attributes[None][0]-jet_speed, 0)
+            jet_attributes[None] = [tmp, jet_attributes[None][1]]
+            print(jet_attributes)
+        elif gui.event.key == 'd':
+            tmp = ti.math.min(jet_attributes[None][0]+jet_speed, 1)
+            jet_attributes[None] = [tmp, jet_attributes[None][1]]
+            print(jet_attributes)
+    mouse = gui.get_cursor_pos()
+    jet_x = jet_attributes[None][0]
+    #Compute the angle between the cursor and the jet's location
+    mouse_jet_angle = ti.math.acos((mouse[0]- jet_x)/ti.math.sqrt(ti.math.max(1e-4,(mouse[0]- jet_x)**2 + mouse[1]**2)))
+    if mouse_jet_angle > jet_attributes[None][1]:
+        jet_attributes[None][1] = ti.math.min(ti.math.pi, jet_attributes[None][1]+jet_angular_speed)
+    elif mouse_jet_angle < jet_attributes[None][1]:
+        jet_attributes[None][1] = ti.math.max(jet_attributes[None][1]-jet_angular_speed, 0)
+    #LMB is pressed, shoot water
+    if gui.is_pressed(ti.GUI.LMB):
+        particle_shot_counter = shoot_length
+    if particle_shot_counter > 0 :
+        batch = 20
+        start = random_pos() * (n_particles - 400 - batch) + 400 #TODO Parametrize number of ball particles
+        for i in range(int(start), int(start + batch)):
+            new_x = random_pos()*jet_r
+            x[i] = [new_x + jet_attributes[None][0],1e-4] 
+            v[i] = [jet_power*ti.math.cos(jet_attributes[None][1]), jet_power*ti.math.sin(jet_attributes[None][1])]
+        particle_shot_counter -= 1
+    gui.circle((mouse[0], mouse[1]), color=0x336699, radius=15)
+    attractor_pos[None] = [mouse[0], mouse[1]]
+    attractor_strength[None] = 0
+    if gui.is_pressed(ti.GUI.RMB):
+        attractor_strength[None] = -1 #shoot air
+    for s in range(int(2e-3 // dt)):
+        #pass
+        substep()
+    gui.circles(x.to_numpy(),
+                radius=1.5,
+                palette=[0x068587, 0xED553B, 0xEEEE00],
+                palette_indices=material)
+
+    # Change to gui.show(f'{frame:06d}.png') to write images to disk
+    gui.show()
